6048d1df86f7803c50fcc1066711d884ebaff863
Franz Reitmayer <franz.reitmayer@reitmayer.eu>
2024-03-29 00:09:08 +0100

    progress gameboard

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,11 +7,37 @@ const initialGameBoard = [
 ]
 
 export default function Gameboard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    
+    const handleSelectSquare = (rowIndex, colIndex) => {
+/*         setGameBoard( (prevGameBoard) => {            
+            const updatedBoard = [
+                [null, null,null],
+                [null, null,null],
+                [null, null,null]
+            ];
+            // const updatedGameBoard = prevGameBoard;
+            // updatedGameBoard[rowIndex][colIndex] = 'X';
+            // return updatedGameBoard;
+            return  updatedBoard;
+        });  */
+        setGameBoard(
+            (prevBoard) => {
+                const updatedBoard = [...prevBoard.map(innerArray => [...innerArray])];
+                updatedBoard[rowIndex][colIndex] = 'X';
+                return updatedBoard;
+            }
+        );
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
-                    {row.map((col, colIndex) => <li><button>{col}</button></li>)}
+                    {row.map((col, colIndex) => 
+                    <li key={colIndex}>
+                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{col}</button>
+                    </li>)}
                 </ol>
             </li>)}
         </ol>
