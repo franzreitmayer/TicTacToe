2bbec98e1bbb8542b7e04224a069f0c707e4180f
franz.reitmayer <franz.reitmayer@reitmayer.eu>
2024-03-28 18:09:31 +0100

    added two way binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,26 @@
 import {useState} from 'react';
 
 export default function Player({ name, symbol }) {
+    const [playerName, setPlayerName] = useState(name);
+    
+    const [isEditing, setIsEditing ] = useState(false);
+    
+    
+    const handlePlayerNameChange = (event) => {
+        setPlayerName( event.target.value );
+    };
+
+    const handleButton = () => {
+        setIsEditing( (editing) => !editing );
+    }
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                { !isEditing ? <span className="player-name">{playerName}</span> : <input onChange={handlePlayerNameChange} type="text" value={playerName} /> }
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleButton}>{isEditing ? "Save" : "Edit" }</button>
         </li>
 
     );
